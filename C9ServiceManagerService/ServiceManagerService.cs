b4fbb43ef54aa4dceebd714b9132c66f307d44e9
Kyle Wilson <kyle@ninecrows.com>
2017-08-26 10:55:14 -0400

    Moved repetition rate to class as a member variable and logging of startup of timer

--- C9ServiceManagerService/ServiceManagerService.cs
+++ C9ServiceManagerService/ServiceManagerService.cs
@@ -12,6 +12,11 @@ namespace C9ServiceManagerService
 {
     public partial class ServiceManagerService : ServiceBase
     {
+        /// <summary>
+        /// Milliseconds per tick for making adjustments.
+        /// </summary>
+        private int runRate = 20000;
+
         public ServiceManagerService()
         {
             InitializeComponent();
@@ -20,11 +25,22 @@ namespace C9ServiceManagerService
         protected override void OnStart(string[] args)
         {
             EventLog.WriteEntry("Service Manager Service Starting Up...");
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = runRate;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+
+            EventLog.WriteEntry($"Timer intialized at {runRate / 1000} Seconds");
         }
 
         protected override void OnStop()
         {
             EventLog.WriteEntry("Service Manager Service Stopping...");
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            EventLog.WriteEntry("Ping");
         }
     }
 }
